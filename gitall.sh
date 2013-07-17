7d3355342766286638f58ce42994199858dc078c
openkava <gmail@openkava.com>
2013-07-17 20:42:10 +0800

    dd

--- gitall.sh
+++ gitall.sh
@@ -3,7 +3,7 @@ git clone https://github.com/gmoledina/GMGridView.git
 git clone https://github.com/kstenerud/KSReachability.git
 git clone https://github.com/lexrus/LTUpdate.git
 git clone https://github.com/mattgemmell/MGImageUtilities.git
-#git clone https://github.com/jverkoey/nimbus.git
+git clone https://github.com/jverkoey/nimbus.git
 git clone https://github.com/Sephiroth87/ODRefreshControl.git
 git clone https://github.com/soffes/sskeychain.git
 git clone https://github.com/soffes/sstoolkit.git
@@ -16,3 +16,18 @@ git clone https://github.com/nicklockwood/ViewUtils.git
 git clone https://github.com/m1entus/WCAlertView.git
 git clone https://github.com/nicklockwood/iCarousel.git
 git clone https://github.com/atgreen/libffi.git
+
+https://github.com/AlexBarinov/UIBubbleTableView.git
+
+git clone https://github.com/samsoffes/sscatalog.git
+
+https://github.com/Redth/PushSharp.git
+
+https://github.com/TheRealKerni/HockeyKit
+
+https://github.com/pkluz/PKRevealController
+
+https://github.com/steipete/PSStackedView
+
+https://github.com/ptshih/PSCollectionView
+https://github.com/sobri909/MGBox2
