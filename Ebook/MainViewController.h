70180b7f6cccc37c7cf09e9e72bfe7cc24243a00
openkava <openkava@gmail.com>
2013-07-29 18:52:39 +0800

    20130729

---
+++ Ebook/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  MainViewController.h
+//  Ebook
+//
+//  Created by ydf on 13-7-17.
+//  Copyright (c) 2013年 openkava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <IIViewDeckController.h>
+
+@interface MainViewController : UIViewController //IIViewDeckController
+@property (weak, nonatomic) IBOutlet UIView *pageContainer;
+
+@end
